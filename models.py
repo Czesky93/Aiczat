92d2e63e952a0fc980e8d19caf697d7c306021a4
Czesky93 <rafal.kluczewski.info@gmail.com>
2025-01-27 23:52:51 +0100

    Add files via upload

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -7,3 +8,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    action = db.Column(db.String(200), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
